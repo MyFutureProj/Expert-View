ba2e8f81c7652f27181dc29fb54fc9404fdaf143
Sagar Trivedi <sagar.trivedi@zymr.com>
2023-01-03 17:54:06 +0530

    JWT token changes and authenticate all apis

--- user-auth-service/src/main/java/com/camelcase/userauthservice/constants/UserServiceConstant.java
+++ user-auth-service/src/main/java/com/camelcase/userauthservice/constants/UserServiceConstant.java
@@ -2,4 +2,5 @@ package com.camelcase.userauthservice.constants;
 
 public class UserServiceConstant {
     public final static String PRODUCTS = "/products";
+    public final static String JWT_ISSUER = "EXPERT_VIEW";
 }
