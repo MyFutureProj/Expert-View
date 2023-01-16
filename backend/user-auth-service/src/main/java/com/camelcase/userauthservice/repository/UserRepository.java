a986d195fe2e24951e9d3384b9f40806cb1624a0
sagartrivedi <sagartrivedi26@gmail.com>
2023-01-16 18:13:54 +0530

    added user-auth module

---
+++ backend/user-auth-service/src/main/java/com/camelcase/userauthservice/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.camelcase.userauthservice.repository;
+
+import com.camelcase.userauthservice.model.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+    Optional<User> findByEmail(String email);
+}
